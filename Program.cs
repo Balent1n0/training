77bcf390f794c94d31c7f6146b2487cc8348da01
Valentin Evdokimov <evdokimov@byndyusoft.com>
2022-08-24 12:06:55 +0300

    pr

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Calc
+{
+    public static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Choose the operatin: #1 - 'addition', #2 - 'subtraction', #3 - 'multiplication': ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        int d;
+        switch (c)
+        {
+            case 1:
+                Console.WriteLine($"Decision: {a} + {b} = {d = a + b}");
+                break;
+            case 2:
+                Console.WriteLine($"Decision: {a} - {b} = {d = a - b}");
+                break;
+            case 3:
+                Console.WriteLine($"Decision: {a} * {b} = {d = a * b}");
+                break;
+            default:
+                Console.WriteLine("Undefind number of operation.");
+                break;
+        }
+    }
+}
